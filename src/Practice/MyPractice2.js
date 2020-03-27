292fcdb45ee5acfc5ec4c7833943700c31c64cd0
Rachel4858 <rachel458458@gmail.com>
2020-03-27 14:25:05 +0900

    Resolve password checking error

--- src/Practice/MyPractice2.js
+++ src/Practice/MyPractice2.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
 
 class MyPractice2 extends Component {
+
   state = {
-    email:'',
+    email: '',
     username: '',
     password: '',
-    correctPw:false,
     list: []
   };
-  idNum = 1;
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState, this.state);
-    if (prevState.correctPw) {
-      this.setState({
-        correctPw: false
-      });
-    }
-  }
+  idNum = 1;
+  correctPw = false;
 
   handleChange = (e) => {
     const {name, value} = e.target; // e.target의 name은 username or password일것이기 때문에
@@ -26,70 +19,51 @@ class MyPractice2 extends Component {
       [name]: value,
     });
 
-    // this.isSafe(e);
-    if([name][0] === "password") {
-      if([value][0].length >= 6) {
-        console.log("6자리이-통과");
-        this.setState({
-          correctPw: true
-        })
-      } else if([value][0].length < 6) {
-        this.setState({
-          correctPw: false
-        })
-      }
-    }
-
+    this.isSafe(e);
   };
 
-  // isSafe = (e) => {
-  //   const {name, value} = e.target; // e.target의 name은 username or password일것이기 때문에
-  //
-  //   if([name][0] === "password") {
-  //     if([value][0].length >= 6) {
-  //       console.log("6자리이-통과");
-  //       this.setState({
-  //         correctPw: true
-  //       })
-  //     } else if([value][0].length < 6) {
-  //       this.setState({
-  //         correctPw: false
-  //       })
-  //     }
-  //   }
-  // };
+  isSafe = (e) => {
+    const {name, value} = e.target;
 
-
-  handleAddList = (e) => {
-    const {email, username, password, list} = this.state;
-      this.setState({
-        email:'',
-        username: '',
-        password: '',
-        // 추가버튼 누르면 username,password input창은 다시 빈 상태로 리셋되야 하므로
-        list: list.concat({
-          email, username, password, id: this.idNum
-        })
-      });
-      this.idNum++
+    if ([name][0] === "password") {
+      if ([value][0].length >= 6) {
+        console.log("통과");
+        this.correctPw = true;
+      } else if ([value][0].length < 6) {
+        console.log("불통과-6자리 이상 입력해주세요");
+        this.correctPw = false;
+      }
     }
+  };
 
-  throwError = () => {
-    // alert("비밀번호를 6자리 이상 입력해주세요.")
-
-  }
-
+  handleAddList = () => {
+    const {email, username, password, list} = this.state;
+    this.setState({
+      email: '',
+      username: '',
+      password: '',
+      // 추가버튼 누르면 username,password input창은 다시 빈 상태로 리셋되야 하므로
+      list: list.concat({
+        email, username, password, id: this.idNum
+      })
+    });
+    this.idNum++;
+    this.correctPw = false;
+  };
 
   render() {
-    const {email, username, password, list, correctPw} = this.state;
+    const {email, username, password, list} = this.state;
+
     return (
       <div>
         email : <input type="email" name="email" value={email} onChange={this.handleChange}/>
         username : <input type="text" name="username" value={username} onChange={this.handleChange}/>
         password : <input type="password" name="password" value={password} onChange={this.handleChange}/>
-        <button onClick={correctPw === true ? this.handleAddList : this.throwError} disabled={!correctPw}>click-AddList</button>
+        <button onClick={this.correctPw === true ? this.handleAddList : null}
+                disabled={!this.correctPw}>click-AddList
+        </button>
         <ul>
-          {/*ul안에서 list를 map돌면서 li그릴 예정*/}
+          {/*ul안에서 list를 map돌면서 li그려나갈 예정*/}
           {list.map((value) => {
             return (
               <li keyId={value.id}>
